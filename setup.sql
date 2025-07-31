de264c2f9ccdae1580ad30027e5f4f7ab492073e
josdic1 <joshuadicker@gmail.com>
2025-07-31 15:43:01 -0400

    removing and recreating db

--- setup.sql
+++ setup.sql
@@ -3,18 +3,18 @@ CREATE TABLE songs (
     id INTEGER PRIMARY KEY,
     title TEXT,
     genre TEXT,
-    UNIQUE(title, name)
-)
+    UNIQUE(title, genre)
+);
 
 CREATE TABLE tags (
     id INTEGER PRIMARY KEY,
     title TEXT
-)
+);
 
 CREATE TABLE song_tags (
     id INTEGER PRIMARY KEY,
     song_id INTEGER,
     tag_id INTEGER,
     FOREIGN KEY (song_id) REFERENCES songs (id),
-    FOREIGN KEY (tag_id) REFERENCES tags (id),
+    FOREIGN KEY (tag_id) REFERENCES tags (id)
 )
